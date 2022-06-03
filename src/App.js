f874cff1103fb80d0903af5c3432f8ed3f8a36cc
Javlon <59303287+javlona@users.noreply.github.com>
2022-06-02 21:17:20 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [value, setValue] = useState('');
+  const [results, setResults] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      });  
+  }, [])
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setUsers(results);
+        setLoading(false);
+      } );
+
+  }, [value])
+
+  const url = `https://api.github.com/search/users?q=${value}`;
+  
+  console.log(users)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-title">Github users</h1>
+      <input type='search' placeholder='Search...' onChange={(e) => setValue(e.target.value)} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className='users'>
+          {users?.map(user => (
+            <li key={user.id}>
+              <img src={user.avatar_url} alt={user.login} />
+              <a href={user.html_url}>{user.login}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+
     </div>
   );
 }
